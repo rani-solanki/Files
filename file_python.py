7fd97710c94863d8ea1497e612ef88e838e6cd91
Ranisolanki <59246445+rani-solanki@users.noreply.github.com>
2020-11-16 09:48:53 +0530

    Add files via upload

--- file_python.py
+++ file_python.py
@@ -1,1 +1,6 @@
-people1.txt
+# file opening example in Python
+fo = open("sample.txt", "wb")
+    print ("File Name: ", fo.name)
+    print ("Mode of Opening: ", fo.mode)
+    print ("Is Closed: ", fo.closed)
+    print ("Softspace flag : ", fo.softspace)
